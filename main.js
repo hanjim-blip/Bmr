b1a118513c4250d953ac322f4a8b74b4aa4a39c9
hanjim-blip <videyhanjim@gmail.com>
2026-08-15 18:11:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const shareBtn = document.getElementById("shareBtn");
 const shareLinks = document.getElementById("shareLinks");
 
 shareBtn.onclick = () => {
-    window.open("https://vt.tokopedia.com/t/ZS9hF44oJNj34-VNvrA/", "_blank")
+    window.open("https://vt.tokopedia.com/t/ZS9kAo8vcUc6R-cWIX2/", "_blank")
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://vt.tokopedia.com/t/ZS9rT2ohmF2So-WQno4/");
+    window.open("https://vt.tokopedia.com/t/ZS9kAoYH9BMqu-4cCIf/");
 }
 
 function openFacebookPage() {
-    window.open("https://vt.tokopedia.com/t/ZS9hF44oJNj34-VNvrA/");
+    window.open("https://vt.tokopedia.com/t/ZS9kAo8vcUc6R-cWIX2/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
